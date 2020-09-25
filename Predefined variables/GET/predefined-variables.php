fdf844d38c34133980af73faef86045e6211d84e
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-25 07:46:11 +0200

    GET and POST

--- Predefined variables/GET/predefined-variables.php
+++ Predefined variables/GET/predefined-variables.php
@@ -1,28 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Predefined variables</title>
 </head>
+
 <body>
-<div class="container">
-    <div class="menu">
-        <div><a href="?page=java">Java</a></div>
-        <div><a href="?page=php">PHP</a></div>
-        <div><a href="?page=mysql">MySql</a></div>
+    <div>
+        <h1>Baner</h1>
     </div>
     <div class="container">
-    <?php
+        <div class="menu">
+            <div><a href="?page=java">Java</a></div>
+            <div><a href="?page=php">PHP</a></div>
+            <div><a href="?page=mysql">MySql</a></div>
+        </div>
+        <div class="container">
+            <?php
         echo "---------/ \$_GET /--------- <br/>";
 
         if(isset($_GET['page']))
         {
+            $allowed_pages = array("java", "mysql", "php");
 
             $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
             
             if(!empty($page))
             {
+
+                if(!in_array($page, $allowed_pages))
+                  echo "Taka strona nie iestnieje";
+                 else
+
+                /* 
+                    in_array - sprawdza czy jakaś wartość znajduje się w tablicy
+                    is_file - sprawdza czy dany plik istnieje
+                    include - dołącza
+                    empty - sprawdza czy jest pusto
+                */
                 if(is_file($page.".html"))
                     include($page.".html");
                 else 
@@ -36,10 +53,11 @@
 
         echo "<br/>---------//---------<br/>";
     ?>
+        </div>
     </div>
-</div>
 
 
- 
+
 </body>
+
 </html>
