beeb4a3a0bb5ec30df2f86d5ac36e88901cbf1d6
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-29 18:56:05 +0200

    correct

--- Predefined variables/POST/odbierz.php
+++ Predefined variables/POST/odbierz.php
@@ -16,6 +16,11 @@ session_start(); // $_SESSION['nzawa'] = wartość;
 
 <body>
     <?php
+    if (isset($_GET['akcja']) && $_GET['akcja'] == 'wyloguj')
+    {
+        $_SESSION['zalogowany'] = 0;
+        echo "zostałeś pomyślnie wylogowany<br/>";
+    }
     if((isset($_POST['login']) && isset($_POST['hasło'])) || $_SESSION['zalogowany'] == 1)
     {
         if((!empty($_POST['login']) && !empty($_POST['hasło'])) || $_SESSION['zalogowany'] == 1)
@@ -23,11 +28,15 @@ session_start(); // $_SESSION['nzawa'] = wartość;
           $login =  filter_var($_POST['login'], FILTER_SANITIZE_STRING);
           $password = filter_var($_POST['hasło'], FILTER_SANITIZE_STRING);
 
-          if($login == "Armon" && $password == "abc" || $_SESSION['zalogowany'] == 1)
+          if(($login == "Armon" && $password == "abc") || $_SESSION['zalogowany'] == 1)
           {
               echo "Gratulacje zalogowałeś się na konto: ".$login."<br/>";
               echo "PANEL ADMINISTRACYJNY <br/>";
-              $_SESSION['zalogowany'];
+              echo "<a href='index.php'>Odśwież</a><br/>";
+              echo "<a href='index.php?akcja=wyloguj'>Wyloguj się</a><br/>";
+              
+              $_SESSION['zalogowany'] = 1;
+
           }
           else
           echo "Nie podałeś loginu lub hasła. Spróbuj ponownie <a href='index.php'>tutaj</a>";
