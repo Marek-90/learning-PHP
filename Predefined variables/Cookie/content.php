b8f65de2a69fb9bbb2f9bc2fcc98829daca7be76
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-30 06:50:14 +0200

    cookie start

---
+++ Predefined variables/Cookie/content.php
@@ -0,0 +1,8 @@
+<?php
+        
+        echo "Gratulacje zalogowałeś się na konto: ".$_SESSION['login']."<br />";
+        echo "PANEL ADMINISTRACYJNY<br />";
+        echo "<a href='index.php'>Odśwież</a><br />";
+        echo "<a href='index.php?akcja=wyloguj'>Wyloguj się</a><br />";
+                        
+?>
